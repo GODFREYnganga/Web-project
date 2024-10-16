0dc058866f657c9204c5857651c8aabf16107172
GODFREYnganga <godfreynganga50@gmail.com>
2024-10-16 15:38:04 +0300

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+const javaclass=document.getElementById('colorButton')
+
+javaclass.addEventListener('click', ()=>{
+
+    javaclass.style.backgroundColor = javaclass.style.backgroundColor == 'blue' ? '#4CAF50' : 'blue';
+
+});
